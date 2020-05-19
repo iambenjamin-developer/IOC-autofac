fdbee8f20a760f0a2b6a169d92b9411051c83390
Your Name <you@example.com>
2020-05-19 15:24:19 -0300

    dar de baja usuario

---
+++ Benjamin.PracticoMVC.WebApp/Scripts/Custom/vistaListar.js
@@ -0,0 +1,14 @@
+﻿function confirmar(id) {
+
+    alertify.confirm("¿Desea dar de baja al Usuario ID " + id.toString() + "?", function (e) {
+        if (e) {
+            //after clicking OK
+            location.href = "/Usuarios/Eliminar/?id=" + id;
+
+        } else {
+            //after clicking Cancel          
+        }
+    });// fin alertify
+
+    //   <button class="btn btn-danger" onclick="location.href='@Url.Action("Eliminar", "Usuarios",  new { id = obj.ID_USUARIO })'"><i class="fas fa-trash-alt"></i></button>
+}
